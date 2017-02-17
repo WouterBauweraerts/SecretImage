1c4865efcd64f3b6b27db8d6c711b842d4855402
MarijnvandenHeuvel <marijn.vandenheuvel@cegeka.com>
2017-02-17 13:23:21 +0100

    Minor changes

--- src/main/java/com/cegeka/secretImage/controllers/SecretController.java
+++ src/main/java/com/cegeka/secretImage/controllers/SecretController.java
@@ -2,11 +2,14 @@ package com.cegeka.secretImage.controllers;
 
 import com.cegeka.secretImage.services.SecretService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.bind.ValidationException;
 import java.awt.image.BufferedImage;
 
 @RestController
@@ -14,10 +17,17 @@ import java.awt.image.BufferedImage;
 public class SecretController {
     @Autowired
     private SecretService secretService;
+    @Autowired
+    private ExceptionController exceptionController;
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
-    public void addSecretPicture(int pineapple, BufferedImage picture){
-        secretService.addSecretPicture(pineapple, picture);
+    public ResponseEntity addSecretPicture(int pineapple, BufferedImage picture){
+        try {
+            secretService.addSecretPicture(pineapple, picture);
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (UnsupportedOperationException e) {
+            return exceptionController.unsupportedOperationHandler(e);
+        }
     }
 }
