aa648e91b08bea80c6032176358df23fafa22ebf
Wouter Bauweraerts <wouter.bauweraerts@cegeka.com>
2017-02-18 14:24:55 +0100

    some more refactoring in secret service/controller/repo + making collections threadsafe

--- src/main/java/com/cegeka/secretImage/domain/repos/SecretRepo.java
+++ src/main/java/com/cegeka/secretImage/domain/repos/SecretRepo.java
@@ -8,27 +8,27 @@ import java.util.*;
 @Repository
 public class SecretRepo {
 
-    private Map<Integer,SecretPicture> secretPictures;
+    private Map<Integer, SecretPicture> secretPictures;
 
     public SecretRepo() {
-        this.secretPictures = new HashMap<Integer,SecretPicture>();
+        this.secretPictures = Collections.synchronizedMap(new HashMap<Integer, SecretPicture>());
     }
 
-    public void addSecretPicture(SecretPicture secretPicture){
-        secretPictures.put(secretPicture.getPineapple(),secretPicture);
+    public void addSecretPicture(SecretPicture secretPicture) {
+        secretPictures.put(secretPicture.getPineapple(), secretPicture);
     }
 
     public void deleteSecretPicture(int pineapple) {
         secretPictures.remove(pineapple);
     }
 
-    public SecretPicture getRandomSecretPicture() throws UnsupportedOperationException{
+    public SecretPicture getRandomSecretPicture() throws UnsupportedOperationException {
         int size = secretPictures.size();
-        if(size!=0){
+        if (size != 0) {
             throw new UnsupportedOperationException("No secret pictures available.");
         }
         int item = new Random().nextInt(size);
-        return new ArrayList<SecretPicture>(secretPictures.values()).get(item);
+        return Collections.synchronizedList(new ArrayList<>(secretPictures.values())).get(item);
     }
 
     public boolean SecretPictureAlreadyExists(int pineapple) {
