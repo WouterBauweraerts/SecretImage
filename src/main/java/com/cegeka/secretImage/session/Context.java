3a5e82a53231f7ba715aa7e684d0e06a2fea40fc
Wouter Bauweraerts <wouter.bauweraerts@cegeka.com>
2017-02-18 14:20:15 +0100

    some more refactoring in session/Context.java

--- src/main/java/com/cegeka/secretImage/session/Context.java
+++ src/main/java/com/cegeka/secretImage/session/Context.java
@@ -1,14 +1,9 @@
 package com.cegeka.secretImage.session;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.context.annotation.SessionScope;
 
-/**
- * Created by wouterba on 17/02/2017.
- */
+
 @Component
 @SessionScope
 public class Context {
